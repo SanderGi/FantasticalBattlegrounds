4ece59dfbc8d89b96acc7e611a3b49b7beb2ab47
Alexander Metzger <alexander.le@outlook.dk>
2020-05-26 03:01:38 +0000

    ☕️💒 Checkpoint ./public/script.js:12528664/531 ./public/index.html:12528664/37

--- public/script.js
+++ public/script.js
@@ -4,9 +4,8 @@ if (user == null) {
   localStorage.setItem('user', JSON.stringify(user));
 }
 const canvas = document.getElementById('canvas');
-function resize() { canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight); }
-resize();
-document.onresize = resize;
+function resize() { canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight); console.log("resized canvas width: " + canvas.width); }
+window.onresize = () => { resize(); };
 const ctx = canvas.getContext('2d');
 const socket = io();
 
@@ -19,16 +18,14 @@ function join(room) {
   console.log('joined ' + room);
   location.hash = room;
   document.getElementById('game').style.display = 'block';
-  ctx.clearRect(0, 0, canvas.width, canvas.height, 0);
+  resize();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 document.getElementById('create').onclick = () => {
   document.getElementById('game').style.display = 'block';
-  ctx.clearRect(20, 20, 100, 50);
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  ctx.lineTo(500, 500);
-  ctx.stroke();
+  resize();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
 document.getElementById('join').onclick = () => { 
@@ -42,4 +39,8 @@ document.getElementById('profile').onclick = () => {
     user.name = newName;
     localStorage.setItem('user', JSON.stringify(user));
   }
+};
+
+window.onbeforeunload = () => {
+  location.hash = "";
 };
