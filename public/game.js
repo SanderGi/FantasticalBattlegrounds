bb026f2864c8b2537a0fdc50adb2a3921ec64e15
Alexander Metzger <alexander.le@outlook.dk>
2020-05-26 23:47:59 +0000

    🚔✈️ Checkpoint ./public/game.js:12528664/1282

--- public/game.js
+++ public/game.js
@@ -11,29 +11,54 @@ window.onresize = () => { resize(); };
 const ctx = canvas.getContext('2d');
 
 const tileSprite = new Image("https://cdn.glitch.com/00742a89-c4d3-4606-baad-f1fad6586440%2FisometricTiles.png?v=1590463010897");
+const tileWidth = 40, tileHeight = 20;
 
 class World {
   constructor(width, height) {
     this.width = width;
     this.height = height;
     
-    this.tiles = [];
-    while (height--) {
-        this.tiles.push(new Array(width).fill({v: 0, s: 3}));
-    }
+    this.tiles = new Uint8Array(width * height);
+    this.tiles.fill(TileStatus.UPDATED + TileValue.GRASS);
     this.buildings = [];
     this.units = [];
   }
   
   show() {
     let screenPos = {};
-    let tile = {};
+    let tile;
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
         screenPos = this.toScreen(x, y);
-        tile = this.tiles[x, y]
-        this.drawTile(screenPos.x, screenPos.y, );
+        tile = this.tiles[x * this.width + y];
+        this.drawTile(screenPos.x, screenPos.y, tile);
       }
     }
   }
+  
+  drawTile() {
+    
+  }
+  
+  toScreen(x, y) {
+    
+  }
 }
+
+const TileStatus = {
+  UNDISCOVERED: 0,
+  DISCOVERED: 85,
+  UPDATED: 170,
+  VALUES: 85
+}; Object.freeze(TileStatus);
+
+const TileValue = {
+  SELECTED: 0,
+  EMPTY: 1,
+  GRASS: 2,
+  RAINBOW: 3,
+  TREE: 4,
+  DEADTREE: 5,
+  SAND: 6,
+  WATER: 7
+}; Object.freeze(TileValue);
