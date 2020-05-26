79f83c34cbf9d3e1e75a5358d7529e2f0efb04ae
Alexander Metzger <alexander.le@outlook.dk>
2020-05-26 02:25:38 +0000

    💡⛲️ Checkpoint ./public/style.css:12528664/68 ./public/script.js:12528664/239 ./public/index.html:12528664/210

--- public/script.js
+++ public/script.js
@@ -1,4 +1,10 @@
+var user = {};
 const socket = io();
+
 socket.on('connect', () => {
   console.log("SID: " + socket.id);
 });
+
+document.getElementById('profile').onclick = () => {
+  var newName = prompt("Type new username below:", user.name ? user.name : "Harry Potter");
+};
