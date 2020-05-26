b9600b8989e0363ef0b3ab65b89313cc725a9557
Alexander Metzger <alexander.le@outlook.dk>
2020-05-26 02:49:37 +0000

    🏀🚵 Checkpoint ./public/script.js:12528664/661 ./public/index.html:12528664/59 ./public/style.css:12528664/15

--- public/script.js
+++ public/script.js
@@ -3,6 +3,11 @@ if (user == null) {
   user = { name: "Harry Potter" };
   localStorage.setItem('user', JSON.stringify(user));
 }
+const canvas = document.getElementById('canvas');
+function resize() { canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight); }
+resize();
+document.onresize = resize;
+const ctx = canvas.getContext('2d');
 const socket = io();
 
 socket.on('connect', () => {
@@ -14,7 +19,17 @@ function join(room) {
   console.log('joined ' + room);
   location.hash = room;
   document.getElementById('game').style.display = 'block';
+  ctx.clearRect(0, 0, canvas.width, canvas.height, 0);
 }
+
+document.getElementById('create').onclick = () => {
+  document.getElementById('game').style.display = 'block';
+  ctx.clearRect(20, 20, 100, 50);
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(500, 500);
+  ctx.stroke();
+};
 
 document.getElementById('join').onclick = () => { 
   var room = prompt("Please enter the room id.", "8duawidlda");
